7233535eb0643dad2975b28b0d0330c1ab638107
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-02-16 00:19:35 +0100

    Upgrade to next v12 and changle font handling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@emotion/react';
 
 import 'styles/reset.css';
-import 'styles/font.css';
 import { theme } from 'theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
