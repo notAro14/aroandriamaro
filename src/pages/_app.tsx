48f33903a01b75844e68eb048fe731b27ff7040c
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-08-18 02:22:07 +0200

    Add support for system theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,26 @@
+import { ThemeProvider } from "next-themes"
+
 import "src/styles/reset.css"
 import "src/styles/fonts.css"
 
 import type { AppPropsWithLayout } from "src/types"
 import PageLayout from "src/shared/layout/page-layout"
+import { light, dark } from "src/lib/stitches.config"
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout =
     Component.getLayout ?? ((page) => <PageLayout>{page}</PageLayout>)
-  return <>{getLayout(<Component {...pageProps} />)}</>
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      value={{
+        light,
+        dark,
+      }}
+    >
+      {getLayout(<Component {...pageProps} />)}
+    </ThemeProvider>
+  )
 }
 export default MyApp
