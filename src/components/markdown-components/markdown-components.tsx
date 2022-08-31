7dcb501ee1592a969fde0278ea844964138144c9
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-08-31 21:28:46 +0200

    Refactor MDX components

--- src/components/markdown-components/markdown-components.tsx
+++ src/components/markdown-components/markdown-components.tsx
@@ -3,104 +3,75 @@ import Heading from "src/ui/heading"
 import Link from "src/ui/link"
 import CodeBlock from "src/components/code-block"
 import Text from "src/ui/text"
+import { styled } from "src/themes/stitches.config"
 
 type HeadingProps = Parameters<typeof Heading>[0]
 type TextProps = Parameters<typeof Text>[0]
 type LinkProps = Parameters<typeof Link>[0]
 type ListProps = Parameters<typeof List>[0]
 
+const MDXHeading = styled(Heading, {
+  marginBottom: "$md",
+  marginTop: "$xl",
+  variants: {
+    fontSize: {
+      "2xl": {
+        fontSize: "$2xl",
+      },
+      xl: {
+        fontSize: "$xl",
+      },
+      lg: {
+        fontSize: "$lg",
+      },
+    },
+  },
+})
+
+const MDXParagraph = styled(Text, {
+  marginTop: "$md",
+  marginBottom: "$md",
+})
+
+const MDXEm = styled((props: TextProps) => <Text as="em" {...props} />, {
+  color: "$text-lo",
+  fontFamily: "$primary",
+})
+
+const MDXLink = styled(
+  (props: LinkProps) => (
+    <Link target="_blank" rel="noopener noreferrer" {...props} />
+  ),
+  {
+    color: "$text-lo",
+    fontWeight: 200,
+  }
+)
+
+const MDXList = styled(
+  (props: ListProps) => <List as="ul" spacing="xs" {...props} />,
+  {
+    fontSize: "$lg",
+    fontFamily: "$primary",
+    fontWeight: 200,
+    listStyleType: "disc",
+    color: "$text-functional",
+    paddingLeft: "$md",
+    marginTop: "$md",
+    marginBottom: "$md",
+  }
+)
+
 export const MARKDOWN_COMPONENTS = {
   code: CodeBlock,
-  h1: (props: HeadingProps) => (
-    <Heading
-      as="h1"
-      css={{
-        marginBottom: "$md",
-        marginTop: "$xl",
-        fontSize: "$2xl",
-      }}
-      {...props}
-    />
-  ),
-  h2: (props: HeadingProps) => (
-    <Heading
-      as="h2"
-      css={{
-        marginBottom: "$md",
-        marginTop: "$xl",
-        fontSize: "$xl",
-      }}
-      {...props}
-    />
-  ),
-  h3: (props: HeadingProps) => (
-    <Heading
-      as="h3"
-      css={{
-        marginBottom: "$md",
-        marginTop: "$xl",
-        fontSize: "$lg",
-      }}
-      {...props}
-    />
-  ),
-  h4: (props: HeadingProps) => (
-    <Heading
-      as="h4"
-      css={{
-        marginBottom: "$md",
-        marginTop: "$xl",
-        fontSize: "$lg",
-      }}
-      {...props}
-    />
-  ),
-  p: (props: TextProps) => (
-    <Text
-      css={{
-        marginTop: "$md",
-        marginBottom: "$md",
-      }}
-      {...props}
-    />
-  ),
-  em: (props: TextProps) => (
-    <Text
-      as="em"
-      css={{
-        color: "$text-lo",
-        fontFamily: "$primary",
-      }}
-      {...props}
-    />
-  ),
-  a: (props: LinkProps) => (
-    <Link
-      href={props.href}
-      css={{
-        color: "$text-lo",
-        fontWeight: 200,
-      }}
-      {...props}
-    />
-  ),
-  ul: (props: ListProps) => (
-    <List
-      as="ul"
-      spacing="xs"
-      css={{
-        fontSize: "$lg",
-        fontFamily: "$primary",
-        fontWeight: 200,
-        listStyleType: "disc",
-        color: "$text-functional",
-        paddingLeft: "$md",
-        marginTop: "$md",
-        marginBottom: "$md",
-      }}
-      {...props}
-    />
-  ),
+  h1: (props: HeadingProps) => <MDXHeading as="h1" fontSize="2xl" {...props} />,
+  h2: (props: HeadingProps) => <MDXHeading as="h2" fontSize="xl" {...props} />,
+  h3: (props: HeadingProps) => <MDXHeading as="h3" fontSize="lg" {...props} />,
+  h4: (props: HeadingProps) => <MDXHeading as="h4" fontSize="lg" {...props} />,
+  p: MDXParagraph,
+  em: MDXEm,
+  a: MDXLink,
+  ul: MDXList,
 }
 
 export default MARKDOWN_COMPONENTS
