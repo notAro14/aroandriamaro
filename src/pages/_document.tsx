2f37c67fd8b4a1eb9c0e6622477c61b446097ba9
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2022-10-11 13:23:48 +0200

    Add analytics even in dev to avoid conditional

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -61,14 +61,12 @@ class MyDocument extends Document {
             dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
           {/* umami analytics */}
-          {process.env.NODE_ENV === "production" ? (
-            <script
-              async
-              defer
-              data-website-id="adf5bced-1a27-4505-b0d1-cc6db19a4313"
-              src="https://umami-production-dae4.up.railway.app/umami.js"
-            ></script>
-          ) : null}
+          <script
+            async
+            defer
+            data-website-id="adf5bced-1a27-4505-b0d1-cc6db19a4313"
+            src="https://umami-production-dae4.up.railway.app/umami.js"
+          ></script>
         </Head>
         <body>
           <Main />
