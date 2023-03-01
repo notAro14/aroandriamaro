54156902c3343c2a25a0639442409e259dfd062d
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2023-03-02 00:37:35 +0100

    Add proper manifest
    With PWA builder

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,71 +2,49 @@ import Document, { Html, Head, Main, NextScript } from "next/document"
 
 import { getCssText } from "src/themes/stitches.config"
 
-const FontPreload = () => {
-  return (
-    <>
-      <link
-        rel="preload"
-        href="/fonts/metropolis/metropolis.extra-light.otf"
-        crossOrigin="true"
-        as="font"
-      />
-      <link
-        rel="preload"
-        href="/fonts/luckiest-guy/luckiest-guy.regular.ttf"
-        crossOrigin="true"
-        as="font"
-      />
-    </>
-  )
-}
-
-const FaviconLinks = () => {
-  return (
-    <>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-      <meta name="msapplication-TileColor" content="#da532c" />
-    </>
-  )
-}
-
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <FontPreload />
-          <FaviconLinks />
-          <meta name="theme-color" content="#006adc" />
+          {/* Theme color */}
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: dark)"
+            content="#52a9ff"
+          />
+          <meta
+            name="theme-color"
+            media="(prefers-color-scheme: light)"
+            content="#006adc"
+          />
+          {/* Fonts */}
+          <link
+            rel="preload"
+            href="/fonts/metropolis/metropolis.extra-light.otf"
+            crossOrigin="true"
+            as="font"
+          />
+          <link
+            rel="preload"
+            href="/fonts/luckiest-guy/luckiest-guy.regular.ttf"
+            crossOrigin="true"
+            as="font"
+          />
+          {/* PWA */}
+          <link rel="manifest" href="/manifest.json" />
+          {/* Favicon */}
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          {/* Stitches */}
           <style
             id="stitches"
             dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
-          {/* umami analytics */}
-          <script
-            async
-            defer
-            data-website-id="adf5bced-1a27-4505-b0d1-cc6db19a4313"
-            src="https://umami-production-dae4.up.railway.app/umami.js"
-          ></script>
         </Head>
         <body>
           <Main />
