76d6abfeac87ade2a8310ad679e3640c990d609d
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2023-03-02 14:16:45 +0100

    Implement spacer component with vanilla extract

--- src/components/Spacer/Spacer.tsx
+++ src/components/Spacer/Spacer.tsx
@@ -1,41 +1,9 @@
-import { styled, theme } from "src/themes/stitches.config"
+import { size, Size } from "./Spacer.css"
 
-export const Spacer = styled("span", {
-  display: "block",
-  variants: {
-    size: {
-      "4xl": {
-        height: theme.space["4xl"],
-      },
-      "3xl": {
-        height: theme.space["3xl"],
-      },
-      "2xl": {
-        height: theme.space["2xl"],
-      },
-      xl: {
-        height: theme.space.xl,
-      },
-      lg: {
-        height: theme.space.lg,
-      },
-      md: {
-        height: theme.space.md,
-      },
-      sm: {
-        height: theme.space.sm,
-      },
-      xs: {
-        height: theme.space.xs,
-      },
-      xxs: {
-        height: theme.space.xxs,
-      },
-    },
-  },
-  defaultVariants: {
-    size: "md",
-  },
-})
+interface Props {
+  size?: Size
+}
 
-export default Spacer
+export default function Spacer(props: Props) {
+  return <div className={size[props.size ?? "md"]} />
+}
