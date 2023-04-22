5435ac889eaae3f13a63e7f65dc652f4fb383227
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2023-04-22 16:02:08 +0200

    Set select theme font color

--- src/component/ThemeSelect/ThemeSelect.css.ts
+++ src/component/ThemeSelect/ThemeSelect.css.ts
@@ -4,6 +4,7 @@ import { vars } from "src/themes/theme.css"
 export const select = style({
   background: "none",
   border: "none",
+  color: vars.colors["text-functional-low"],
   fontFamily: vars.fonts.code,
   fontSize: vars.fontSizes.sm,
 })
