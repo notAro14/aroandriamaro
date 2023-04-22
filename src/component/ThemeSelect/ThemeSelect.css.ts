044354f71fbf5f763404252a037dd7f0a532f8c0
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2023-04-22 15:25:29 +0200

    Change theme toggle to select

--- src/component/ThemeSelect/ThemeSelect.css.ts
+++ src/component/ThemeSelect/ThemeSelect.css.ts
@@ -1,20 +1,9 @@
 import { style } from "@vanilla-extract/css"
 import { vars } from "src/themes/theme.css"
-import { text } from "src/component/Text/Text.css"
 
-export const button = style([
-  {
-    border: "none",
-    background: "none",
-    padding: vars.space.xs,
-    textTransform: "uppercase",
-    ":hover": {
-      cursor: "pointer",
-    },
-  },
-  text({ size: "sm" }),
-])
-
-export const icon = style({
-  color: vars.colors["text-vibrant-low"],
+export const select = style({
+  background: "none",
+  border: "none",
+  fontFamily: vars.fonts.code,
+  fontSize: vars.fontSizes.sm,
 })
--- src/component/ThemeSelect/ThemeSelect.tsx
+++ src/component/ThemeSelect/ThemeSelect.tsx
@@ -1,31 +1,32 @@
 import { FC } from "react"
-import { SunIcon, MoonIcon } from "@radix-ui/react-icons"
 
 import useIsBrowser from "src/hooks/use-is-browser"
-import useThemeSwitcher from "src/hooks/use-theme-switcher"
 import * as styles from "./ThemeSelect.css"
-import Flex from "src/component/Flex"
+import { useTheme } from "next-themes"
+import Emoji from "src/component/emoji"
 
 const ThemeSelect: FC = () => {
-  const { resolvedTheme, switchTheme } = useThemeSwitcher()
+  const { theme, setTheme } = useTheme()
   const isBrowser = useIsBrowser()
   if (isBrowser === false) return null
 
   return (
-    <Flex align="center" gap="md">
-      <button
-        className={styles.button}
-        aria-label="Toggle theme"
-        onClick={switchTheme}
-      >
-        {resolvedTheme === "light" && (
-          <MoonIcon width={20} height={20} className={styles.icon} />
-        )}
-        {resolvedTheme === "dark" && (
-          <SunIcon width={20} height={20} className={styles.icon} />
-        )}
-      </button>
-    </Flex>
+    <select
+      className={styles.select}
+      value={theme}
+      onChange={(evt) => setTheme(evt.target.value)}
+    >
+      <option value="system">
+        <Emoji symbol="️💻" ariaLabel="System" /> System
+      </option>
+      <option value="light">
+        <Emoji symbol="☀️" ariaLabel="Sun" />
+        Light
+      </option>
+      <option value="dark">
+        <Emoji symbol="🌙" ariaLabel="Moon" /> Dark
+      </option>
+    </select>
   )
 }
 
