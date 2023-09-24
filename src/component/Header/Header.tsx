29003f5c3f651b4e42507637cdd3e65905020b5c
Aro Andriamaro <aro.andriamaro@aworldforus.com>
2023-09-24 11:41:26 +0200

    migrate header to css modules

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,21 +1,15 @@
 import { FC } from "react"
 import Link from "next/link"
 
-import ThemeSelect from "src/component/ThemeSelect"
-import Text from "src/component/Text"
-
-import * as styles from "./Header.css"
+import styles from "./Header.module.scss"
 
 const Header: FC = () => {
   return (
-    <header className={styles.header}>
-      <div className={styles.inner}>
-        <Link href="/" className={styles.goToHome}>
-          <Text family="secondary" lineHeight="1">
-            Aro&apos;s Blog
-          </Text>
+    <header className={styles.root}>
+      <div className={styles["header-inner"]}>
+        <Link href="/" className={styles["go-to-home"]}>
+          Aro&apos;s Blog
         </Link>
-        <ThemeSelect />
       </div>
     </header>
   )
